4003906708857b38b3d54f8361a158b3bd960d9a
Adenilson Prado Raul <adenilson.prado@hotmail.com>
2022-04-04 19:42:01 -0300

    feat: add test file

---
+++ src/test/app.ts
@@ -0,0 +1,10 @@
+import { getTimeNow } from "../core/countdown";
+import { Time } from "../model";
+
+const time: Time = {
+  hours: 25,
+  minutes: 25,
+  seconds: 25,
+};
+
+console.log(getTimeNow());
